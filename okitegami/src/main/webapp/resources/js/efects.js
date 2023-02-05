9bfeb4d21fb88008edb19ddbca02b466022a230f
Okitegami Kyoko <meli_@OkitegamiDell>
2023-02-04 21:59:15 -0600

    Nota: Okitegami trabajando

---
+++ okitegami/src/main/webapp/resources/js/efects.js
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+document.getElementById("body").onscroll=function(){
+    document.getElementById("banner").style="background: rgba(0, 0, 0, .5)";
+};
+
+
