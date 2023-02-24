2c529c32b906867342ab2307ccb402fdf73a2c5e
Okitegami Kyoko <meli_@OkitegamiDell>
2023-02-24 07:51:51 -0600

    Nota: Okitegami trabajando
    Cosas de la vida

--- okitegami/src/main/webapp/WEB-INF/resources/js/efects.js
+++ okitegami/src/main/webapp/WEB-INF/resources/js/efects.js
@@ -2,8 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
-document.getElementById("body").onscroll=function(){
-    document.getElementById("banner").style="background: rgba(0, 0, 0, .5)";
+const menuBtn=document.getElementsByClassName("menu");
+menuBtn.addEventListener("mouseover",verMenu);
+function verMenu(){
+    console.log(menuBtn);
+        if (document.getElementById("menu_web").getAttribute("style")==="display: block;") {
+            document.getElementById("menu_web").style="display:none;";
+            console.log("click_menu"+document.getElementById("menu_web").getAttribute("display"));
+        }else{
+            document.getElementById("menu_web").style="display:block;";
+            console.log("click_menu_visible : "+document.getElementById("menu_web").getAttribute("style"));
+        };
 };
 
-
+let script=document.createElement('script');
+function(){
+  script  
+};
